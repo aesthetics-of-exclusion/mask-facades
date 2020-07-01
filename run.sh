42d4b39b3478a2af7783d494a69406a389b5091e
Bert Spaan <bert.spaan@gmail.com>
2020-07-01 18:25:31 +0200

    stop on error

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR
+set -e
 
-# const SERVICE_KEY=/Users/bert/.google-cloud/streetswipe-aoe-5627f5cc075a.json
 n=$1
 
 for ((i=0; i < n; i++)); do
